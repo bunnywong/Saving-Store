7c1f78b3d0db525c5299e06dc05013ec75a62a23
Bunny <w@bunnywong.com>
2014-11-29 22:53:03 +0800

    Member form - Editing

--- www/catalog/language/zh-HK/account/register.php
+++ www/catalog/language/zh-HK/account/register.php
@@ -6,25 +6,25 @@ $_['heading_title']        = '會員註冊表格';
 // Text
 $_['text_account']         = '我的賬戶';
 $_['text_register']        = '用戶註冊';
-$_['text_account_already'] = '歡迎你申請成為思詩樂 SUZURAN 及大王 GOO.N 媽媽會會員，只要填妥以下表格，即可獲贈試用裝一套(乾棉6片 + 濕棉2包 + 尿片/褲試用裝2片)，更享有網上購物優惠及最新資訊。';
+$_['text_account_already'] = '歡迎你申請成為思詩樂 SUZURAN 及大王 GOO.N 媽媽會會員，只要填妥以下表格，即可獲贈試用裝一套(<strong>乾棉6片 + 濕棉2包 + 尿片/褲試用裝2片</strong>)，更享有網上購物優惠及最新資訊。';
 $_['text_your_details']    = '您的個人資料';
 $_['text_your_address']    = '您的詳細地址';
 $_['text_newsletter']      = '選擇訂閱方式';
 $_['text_your_password']   = '您的賬戶密碼';
-$_['text_agree']           = '我已經閱讀並同意 <a class="colorbox" href="%s" alt="%s"><b>%s</b></a>條款';
+$_['text_agree']           = '本人已閱讀並同意此網站使用 <a class="colorbox" href="%s" alt="%s"><b>條款及隱私權聲明</b></a>。';
 
 // Entry
 $_['entry_firstname']      = '姓名：';
 $_['entry_lastname']       = '您的姓氏：';
-$_['entry_email']          = '電郵地址  (每一個E-MAIL 只可登記一次)：';
-$_['entry_telephone']      = '電話  (預設為訂單電話)：';
+$_['entry_email']          = '電郵地址 (每一個 E-MAIL 只可登記一次)：';
+$_['entry_telephone']      = '電話 (預設為訂單電話)：';
 $_['entry_fax']            = '&nbsp;&nbsp;傳真號碼：';
 $_['entry_account']        = '賬戶：';
 $_['entry_company']        = '&nbsp;&nbsp;公司名稱：';
 $_['entry_customer_group'] = '公司類型：';
 $_['entry_company_id']     = '公司編碼:';
 $_['entry_tax_id']         = '稅務編碼:';
-$_['entry_address_1']      = '中文地址(預設為訂單送貨地址)：';
+$_['entry_address_1']      = '中文地址 (預設為訂單送貨地址)：';
 $_['entry_address_2']      = '&nbsp;&nbsp;地址 2：';
 $_['entry_postcode']       = '&nbsp;&nbsp;郵政編碼：';
 $_['entry_city']           = '所在城市：';
@@ -35,8 +35,8 @@ $_['entry_password']       = '輸入密碼：';
 $_['entry_confirm']        = '確認密碼：';
 
 // Error
-$_['error_exists']         = '警告： 該郵箱地址已被註冊、請換一個電子郵箱！';
-$_['error_firstname']      = '名字必須在1到32字符之間！';
+$_['error_exists']         = '警告： 該電郵地址已被註冊、請換一個電郵地址！';
+$_['error_firstname']      = '姓名必須在1到32字符之間！';
 $_['error_lastname']       = '姓氏必須在1到32字符之間！';
 $_['error_email']          = '無效的郵箱地址！';
 $_['error_telephone']      = '電話號碼必須為3到32字節！';
@@ -49,6 +49,6 @@ $_['error_address_1']      = '地址長度必須在3到128字符之間！';
 $_['error_city']           = '城市名必須在2到128字節之間！';
 $_['error_postcode']       = '郵編必須在2到10字符之間！';
 $_['error_country']        = '請選擇一個國家！';
-$_['error_zone']           = '請選擇地區/省份！';
+$_['error_zone']           = '請選擇地區！';
 $_['error_agree']          = '警告： 您未同意條款 %s！';
 ?>
