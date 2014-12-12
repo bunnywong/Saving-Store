75f60c550c4e0616632f243127f56acb140fb308
Bunny <w@bunnywong.com>
2014-12-12 18:12:38 +0800

    Mail / Admin - Added client tel

--- www/catalog/model/checkout/xorder.php
+++ www/catalog/model/checkout/xorder.php
@@ -564,7 +564,8 @@ class ModelCheckoutXOrder extends Model {
 					$text .= $language->get('text_new_date_added') . ' ' . date($language->get('date_format_short'), strtotime($order_info['date_added'])) . "\n";
 					$text .= $language->get('text_new_order_status') . ' ' . $order_status . "\n\n";
 					// My Script
-					$text .= "客戶電郵: ".$order_info['email']. "\n";
+					$text .= "客戶電話: ".$order_info['telephone']. "\n\n";
+					$text .= "客戶電郵: ".$order_info['email']. "\n\n";
 
 					$text .= $language->get('text_new_products') . "\n";
 
