c9f6ac545136201443c8f5301f7062a344f37592
Bunny <w@bunnywong.com>
2015-01-03 04:53:09 +0800

    Account / MyAddress & Checkout / Add Address - Change validation text

--- www/catalog/language/zh-HK/account/address.php
+++ www/catalog/language/zh-HK/account/address.php
@@ -27,10 +27,10 @@ $_['entry_default']     = '&nbsp;&nbsp;&nbsp;默認地址：';
 // Error
 $_['error_delete']      = '警告： 您必須填寫至少一個地址！';
 $_['error_default']     = '警告： 您不能註銷您的默認地址！';
-$_['error_firstname']   = '姓氏必須在1到32字符之間！';
+$_['error_firstname']   = '請輸入姓名';
 $_['error_lastname']    = '名字必須在1到32字符之間！';
 $_['error_vat']         = 'VAT 號碼不存在！';
-$_['error_address_1']   = '地址必須在3到128字符之間！';
+$_['error_address_1']   = '請輸入地址';
 $_['error_postcode']    = '郵編必須在2到10字符之間！';
 $_['error_city']        = '城市名必須在2到128字符之間！';
 $_['error_country']     = '請選擇一個國家！';
