b4a9db8cb20afbfe09bcbd6f6a1bf29b239d8d0e
Bunny <w@bunnywong.com>
2015-01-17 08:23:58 +0800

    Half & Full reward apply - Completed

--- www/catalog/language/zh-HK/total/reward.php
+++ www/catalog/language/zh-HK/total/reward.php
@@ -1,9 +1,9 @@
 <?php
-// Heading 
+// Heading
 $_['heading_title'] = '使用獎勵積分 (有效 %s)';
 
 // Text
-$_['text_reward']   = '獎勵積分(%s)';
+$_['text_reward']   = '使用積分(%s)';
 $_['text_order_id'] = '訂單號： #%s';
 $_['text_success']  = '成功： 您已經成功使用了獎勵積分！';
 
